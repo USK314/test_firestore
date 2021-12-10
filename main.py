5e46aa308af6339a07c881ae857e639e3bebd9a1
Haru <83204474+USK314@users.noreply.github.com>
2021-12-10 15:27:57 +0900

    Create main.py

---
+++ main.py
@@ -0,0 +1,28 @@
+from fastapi import FastAPI, Form, status
+from fastapi.responses import JSONResponse
+import crud
+
+app = FastAPI()
+
+# ルート
+@app.get("/")
+async def root():
+    return {"message": "this is root"}
+
+# 特定の登録情報を取得
+@app.get("/member")
+async def get_member(uuid: str):
+    member = await crud.get_member(uuid)
+    resp = {
+        "status": "ok",
+        "data": member
+    }
+    return resp
+
+# メンバー登録
+@app.post("/member")
+async def post_member(
+    name: str = Form(...)
+):
+    uuid = await crud.create_member(name)
+    return JSONResponse(content={"status": "ok", "uuid": uuid, "name": name}, status_code=status.HTTP_201_CREATED)
